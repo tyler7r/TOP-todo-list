7620d787d2038d52f861cd65728c96abf5ac13a8
Tyler Randall <tyler7r@gmail.com>
2023-02-22 11:52:54 -0500

    Created projectForm reset after submitting

--- src/new-project.js
+++ src/new-project.js
@@ -20,12 +20,27 @@ submitProjectBtn.addEventListener('click', (e) => {
     e.preventDefault();
     newProjectInfo();
     projectForm.classList.add('hidden');
+    let allInputs = document.querySelectorAll('input');
+    allInputs.forEach((input) => input.value = '');
+    let priorityInput = document.querySelector('#priority');
+    priorityInput.value = 'low';
 })
 
 function newProjectInfo() {
-    let newProject = new Project(projectTitle, description, dueDate, priority);
+    const title = document.querySelector('#projectTitle').value;
+    const description = document.querySelector('#description').value;
+    const dueDate = document.querySelector('#dueDate').value;
+    const priority = document.querySelector('#priority').value;
+    let newProject = new Project(title, description, dueDate, priority);
     addedProjects.push(newProject);
-    newProject = '';
+    // erasePreviousForm(title.value, description, dueDate, priority);
 }  
+
+// function erasePreviousForm (title, description, dueDate, priority) {
+//     title = '';
+//     description = '';
+//     dueDate = '';
+//     priority = '';
+// }
 
 export { addedProjects };
