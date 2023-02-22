9159636b3f4d10ab9ae5f53282e3d97ad9711f31
Tyler Randall <tyler7r@gmail.com>
2023-02-22 09:56:30 -0500

    Create new Project objects with form values

--- src/new-project.js
+++ src/new-project.js
@@ -1,15 +1,33 @@
-export default function Project(title, description, dueDate, priority) {
+export default function addProjectBtn() {
+    let addProjectBtn = document.querySelector('.addProject');
+    addProjectBtn.addEventListener('click', () => {
+        const projectForm = document.querySelector('.projectForm')
+        projectForm.classList.remove('hidden');
+        submitNewProject();
+    })
+}
+
+function Project(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
 }
 
-function addProjectBtn() {
-    let addProjectBtn = document.querySelector('.addProject');
-    addProjectBtn.addEventListener('click', () => {
-        
+function submitNewProject() {
+    const submitProjectBtn = document.querySelector('#submitProject');
+    submitProjectBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        newProjectInfo();
     })
 }
 
-export { addProjectBtn };
+function newProjectInfo() {
+    const title = document.querySelector('#projectTitle').value;
+    const description = document.querySelector('#description').value;
+    const dueDate = document.querySelector('#dueDate').value;
+    const priority = document.querySelector('#priority').value;
+    let newProject = new Project(title, description, dueDate, priority);
+    console.log(newProject);
+    return title, description, dueDate, priority, newProject;
+}
