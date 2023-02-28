8f59f025d1540a57347b163ef905be5571ea2476
Tyler Randall <tyler7r@gmail.com>
2023-02-28 13:56:41 -0500

    Update upcomingSection w/ todos as well

--- src/views/upcoming-view.js
+++ src/views/upcoming-view.js
@@ -1,10 +1,13 @@
 import { parseISO, isThisWeek } from "date-fns";
 import { addedProjects } from "../project-creation/new-project";
+import sortTodoDates, { clearActiveTodos, activeTodos } from "../todo-creation/sort-todo-dates";
 import { renderProjectCards, initializeProjectCardFunctionality } from "./general-view";
+import { initializeTodoCardFunctionality, renderProjectTodos } from "./project-view";
 
 let upcomingProjects = [];
+let upcomingTodos = [];
 
-function upcomingFilter(array) {
+function upcomingProjectsFilter(array) {
     for (let i = 0; i < array.length; i++) {
         let formattedDate = parseISO(array[i].dueDate);
         if (isThisWeek(formattedDate)) {
@@ -15,9 +18,31 @@ function upcomingFilter(array) {
     }
 }
 
+function upcomingTodosFilter(array) {
+    for (let i = 0; i < array.length; i++) {
+        let formattedDate = parseISO(array[i].dueDate);
+        if (isThisWeek(formattedDate)) {
+            upcomingTodos.push(array[i])
+        } else {
+            continue;
+        }
+    }
+}
+
 export default function renderUpcomingSection() {
-    upcomingFilter(addedProjects);
+    upcomingProjectsFilter(addedProjects);
     renderProjectCards(upcomingProjects);
     initializeProjectCardFunctionality();
+    const content = document.querySelector('.content');
+    const todoSection = document.createElement('div');
+    todoSection.classList.add('todos');
+    todoSection.textContent = 'TODOS';
+    content.appendChild(todoSection);
+    sortTodoDates(addedProjects);
+    upcomingTodosFilter(activeTodos);
+    renderProjectTodos(upcomingTodos);
+    initializeTodoCardFunctionality();
     upcomingProjects = [];
+    upcomingTodos = [];
+    clearActiveTodos();
 }
